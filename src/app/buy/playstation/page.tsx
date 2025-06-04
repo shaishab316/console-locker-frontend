3e2ab196762fa4111feeb2585653513a6767b800
hrmrakib <hrmrakibs@gmail.com>
2025-06-04 19:41:44 +0600

    fixed:  low and high filter issue

--- src/app/buy/playstation/page.tsx
+++ src/app/buy/playstation/page.tsx
@@ -510,7 +510,7 @@ const ProductPage: React.FC = () => {
               <div className='h-10 relative flex gap-3'>
                 <select
                   onChange={(e) => setSortBy(e.target.value)}
-                  className='appearance-none w-max md:w-56 px-2.5 py-2 border border-[#101010] rounded-md font-medium text-sm bg-transparent lg:bg-[#FDFDFD] text-[#101010] shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all cursor-pointer overflow-hidden'
+                  className='appearance-none w-44 md:w-56 px-2.5 py-2 border border-[#101010] rounded-md font-medium text-sm bg-transparent lg:bg-[#FDFDFD] text-[#101010] shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all cursor-pointer overflow-hidden'
                 >
                   <option
                     defaultValue={"Sort By"}
